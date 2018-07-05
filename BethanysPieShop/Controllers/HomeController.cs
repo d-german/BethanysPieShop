3392561551cfa3882ec2130cf0684f3968956974
d-german <facmet107@gmail.com>
2018-07-05 14:09:15 -0500

    made some changes

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,8 +22,15 @@ namespace BethanysPieShop.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.Title = "Pie overview";
             var pies = _pieRepository.GetAllPies().OrderBy(p=>p.Name);
-            return View(pies);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+
+            };
+            return View(homeViewModel);
         }
     }
 }
